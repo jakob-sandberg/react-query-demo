d3db0fedaaf5ef29cd253ca8936637e208ddd86d
Jakob <jsandberg_8@hotmail.com>
2021-09-08 09:41:58 +0200

    API wrapper.

--- src/services/API.js
+++ src/services/API.js
@@ -6,21 +6,26 @@
 import axios from "axios";
 import li from "li";
 
-axios.defaults.baseURL = "";
+axios.defaults.baseURL = "http://localhost:4000";
 
 const get = async (endpoint) => {
 	const response = await axios.get(endpoint);
 
-	// parse link header for pagination
-	const link = li.parse(response.headers["link"]);
-
-	// transform data into something nicer
-	return {
-		...link,
-		count,
-		pages,
-		results: response.data,
-	};
+	//return content
+	return response.data;
 };
 
-export default {};
+// Get all books
+const getBooks = async () => {
+	return await get(`/books`);
+};
+
+// Get a single book with a id
+const getBook = async (id) => {
+	return await get(`/books(${id})`);
+};
+
+export default {
+	getBooks,
+	getBook,
+};
