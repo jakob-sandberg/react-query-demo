3ba0b21420c28855461e51ba49a7e4292a54b6e8
Jakob <jsandberg_8@hotmail.com>
2021-09-08 08:42:57 +0200

    Added a new h1.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
+import React from "react";
+import Container from "react-bootstrap/Container";
 
 const HomePage = () => {
 	return (
 		<Container className="py-3">
-			<h1>Welcome!</h1>
+			<h1>This is the bloody demo!!</h1>
 		</Container>
-	)
-}
+	);
+};
 
-export default HomePage
+export default HomePage;
